c454edb886e63d603863d85c9428c3b4499f8324
tchendoh <42325308+tchendoh@users.noreply.github.com>
2024-04-18 10:37:07 -0400

    Initial Commit

---
+++ xoYeet/xoYeetApp.swift
@@ -0,0 +1,17 @@
+//
+//  xoYeetApp.swift
+//  xoYeet
+//
+//  Created by Eric Chandonnet on 2024-04-18.
+//
+
+import SwiftUI
+
+@main
+struct xoYeetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
