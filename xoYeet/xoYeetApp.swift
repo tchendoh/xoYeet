37bbfe5dd608214f04d2506d1efab1191c603826
tchendoh <42325308+tchendoh@users.noreply.github.com>
2024-05-26 10:18:26 -0400

    Merge remote-tracking branch 'refs/remotes/GitHub/main'

--- xoYeet/xoYeetApp.swift
+++ xoYeet/xoYeetApp.swift
@@ -9,9 +9,12 @@ import SwiftUI
 
 @main
 struct xoYeetApp: App {
+    @State var gameViewModel = GameViewModel()
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(gameViewModel)
         }
     }
 }
